df3b78ae29ff74b16936ddc023a463086e9b4c63
KyuDonLim <87511294+KyuDonLim@users.noreply.github.com>
2021-10-05 18:00:15 +0900

    Add files via upload

---
+++ mini/Ticket_cancel.java
@@ -0,0 +1,10 @@
+package test;
+
+
+class Ticket_cancel extends Frame {
+
+	Ticket_cancel(String lbl) {
+		super(lbl);
+	}
+
+}
---
+++ mini/User_info.java
@@ -0,0 +1,10 @@
+package test;
+
+
+class User_info extends Frame {
+
+	User_info(String lbl) {
+		super(lbl);
+	}
+
+}
