02c14af09d6a4eef50d75fc1c7e1957fc4c40045
tyhong98 <jwkim30@naver.com>
2021-10-06 14:36:06 +0900

    Update Reservation_check.java
    abc

--- hong/Reservation_check.java
+++ hong/Reservation_check.java
@@ -1,53 +1,59 @@
 package mini;
 
-import java.awt.Button;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.awt.Container;
-import java.awt.TextArea;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Scanner;
 import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
 
 public class Reservation_check extends JFrame {
 
 	private static JTextArea textArea;
+	public static JButton messageBtn;
 
 	private static Connection conn;
 	private PreparedStatement sfmem, sfmov;
-	private String userId;
 	private ResultSet rs, rm;
-	public String lists = "A";
+	private String lists = "";
 	Frame frm;
 
-	private static String SQL = "SELECT movie_id FROM members";
-	private static String MOV = "SELECT * FROM movie ";
 
 	public Reservation_check() {
 		init();
-
 		conn = Main.makeConnection();
 		try {
+			String SQL = "SELECT movie_id FROM members";
 			sfmem = conn.prepareStatement(SQL);
 			ResultSet rs = sfmem.executeQuery();
 
-			sfmov = conn.prepareStatement(MOV);
-			ResultSet rm = sfmov.executeQuery();
-			
-			while (rs.next()) {
-				String id = rm.getString("M_ID");
-				lists += id;
+			String MOV = "SELECT * FROM movie where m_id = 2";
+			PreparedStatement pstmt = conn.prepareStatement(MOV);
+			ResultSet rm = pstmt.executeQuery();
 
+			while (rm.next()) {
 
+				
+				lists += "영화 제목: " + rm.getString(2) + "\n" + 
+						"영화 시간: " + rm.getString(3).substring(5,16) +"\n" +
+						"상영 관: " + rm.getInt(4) + "관";
+
+				messageBtn.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						JOptionPane.showMessageDialog(null, lists, "Message", JOptionPane.ERROR_MESSAGE);
+					}
+				});
 			}
-
 		} catch (SQLException e2) {
 			JOptionPane.showMessageDialog(null, "개발자의 미슥테이크");
 		}
@@ -56,9 +62,8 @@ public class Reservation_check extends JFrame {
 	private void init() {
 		frm = new Frame("예매조회");
 
-		textArea = new JTextArea(lists);
-		textArea.setBounds(50, 90, 400, 200);
-		frm.add(textArea);
+		messageBtn = frm.setButton("예매조회", 150, 130, 200, 100);
+		frm.add(messageBtn);
 
 		frm.setVisible(true);
 	}
